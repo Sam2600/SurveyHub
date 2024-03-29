bcb11e15ffad85b4b21c18c12718eda6be488afe
Kaung Htet San <kaunghtet.san@brycenmyanmar.com.mm>
2024-03-29 09:42:24 +0630

    remove react project from laravel

--- websurvey-lara-react/src/views/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import { DefaultLayoutComponent } from "../components/DefaultLayoutComponent";
-
-export const Dashboard = () => {
-    return (
-        <DefaultLayoutComponent title="Dashboard">
-            <h1>This is dashbord contents</h1>
-        </DefaultLayoutComponent>
-    );
-};
